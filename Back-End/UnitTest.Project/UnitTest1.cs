1e67f88031cd9775dbaa9bcf7e3d5bb036f090c1
Rodrigo de Almeida Francisco <rdafrancisco@tjsp.jus.br>
2022-09-19 20:18:02 -0300

    Subindo os projetos iniciais.

---
+++ Back-End/UnitTest.Project/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTest.Project
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
